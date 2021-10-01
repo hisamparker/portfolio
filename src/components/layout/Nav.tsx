551e059fcfa0d498587c2b3a0914b1e51bd0f2a3
sam parker <hisamparker@gmail.com>
2021-10-01 09:35:01 +0200

    added hidden prop to dropdown component

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { MdClose, MdMenu } from 'react-icons/md';
 
 const NavStyles = styled.nav`
@@ -11,161 +11,154 @@ const NavStyles = styled.nav`
   width: 100%;
   padding: 1rem 0;
   background: var(--lowlight);
-  ul {
-    max-width: 1200px;
-    margin: 0 auto;
-    width: 90%;
-    text-align: center;
-    li {
-      display: inline-block;
-      border-radius: 8px;
-      transition: 0.3s ease background-color;
-      &:hover {
-        background-color: var(--primary);
-      }
-    }
-    a {
-      display: inline-block;
-      font-family: 'RobotoMono Regular';
-      padding: 1rem 2rem;
-      font-size: 2rem;
-      color: var(--primaryLight);
-    }
-    .active {
-      color: var(--highlight);
-    }
-  }
-  .mobile-menu-icon {
-    position: absolute;
-    right: 1rem;
-    top: 1rem;
-    width: 4rem;
-    cursor: pointer;
-    display: none;
-    outline: none;
-    * {
-      pointer-events: none;
-    }
-  }
-  .navItems .closeNavIcon {
-    display: none;
-  }
   @media only screen and (max-width: 768px) {
     padding: 0;
-    .hide-item {
+    ${({ hidden }) => hidden
+    && css`
       transform: translateY(calc(-100% - var(--top)));
-    }
-    .navItems {
-      --top: 1rem;
-      transition: 0.3s ease transform;
-      background-color: var(--primaryDark);
-      padding: 2rem;
-      max-width: 300px;
-      border-radius: 15px;
-      position: absolute;
-      right: 0;
-      top: var(--top);
-      .closeNavIcon {
-        display: block;
-        width: 3rem;
-        margin: 0 0 0 auto;
-        cursor: pointer;
-        * {
-          pointer-events: none;
-        }
-      }
-      li {
-        display: block;
-        margin-bottom: 1rem;
-      }
-    }
+    `};
   }
 `;
 
 const HamburgerMenu = styled.article`
   position: absolute;
-    right: 1rem;
-    top: 1rem;
-    width: 4rem;
+  right: 1rem;
+  top: 1rem;
+  width: 4rem;
+  cursor: pointer;
+  display: none;
+  outline: none;
+  * {
+    pointer-events: none;
+  }
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const NavItems = styled.ul`
+  max-width: 1200px;
+  margin: 0 auto;
+  width: 90%;
+  text-align: center;
+  @media only screen and (max-width: 768px) {
+    --top: 1rem;
+    transition: 0.3s ease transform;
+    background-color: var(--primaryDark);
+    padding: 2rem;
+    max-width: 300px;
+    border-radius: 15px;
+    position: absolute;
+    right: 0;
+    top: var(--top);
+  }
+`;
+
+const NavItem = styled.li`
+  display: inline-block;
+  border-radius: 8px;
+  transition: 0.3s ease background-color;
+  &:hover {
+    background-color: var(--primary);
+  }
+  a {
+    display: inline-block;
+    font-family: 'RobotoMono Regular';
+    padding: 1rem 2rem;
+    font-size: 2rem;
+    color: var(--primaryLight);
+  }
+  .active {
+      color: var(--highlight);
+  }
+  @media only screen and (max-width: 768px) {
+    display: block;
+    margin-bottom: 1rem;
+  }
+`;
+
+const CloseNav = styled.div`
+  display: none;
+  @media only screen and (max-width: 768px) {
+    display: block;
+    width: 3rem;
+    margin: 0 0 0 auto;
     cursor: pointer;
-    display: none;
-    outline: none;
     * {
       pointer-events: none;
     }
-    @media only screen and (max-width: 768px) {
-      display: block;
-    }
+  }
 `;
 
 const Nav: React.FC = () => {
-  const [showNav, setShowNav] = useState(false);
+  const [showNavDropdown, setShowNavDropdown] = useState(false);
   return (
     <NavStyles>
       <HamburgerMenu
-        onClick={() => setShowNav(!showNav)}
+        onClick={() => setShowNavDropdown(!showNavDropdown)}
         role="button"
-        onKeyDown={() => setShowNav(!showNav)}
+        onKeyDown={() => setShowNavDropdown(!showNavDropdown)}
         tabIndex={0}
       >
         <MdMenu />
       </HamburgerMenu>
-      <ul className={!showNav ? 'navItems hide-item' : 'navItems'}>
-        <div
-          className="closeNavIcon"
-          onClick={() => setShowNav(!showNav)}
+      <NavItems hidden={!showNavDropdown}>
+        <CloseNav
+          onClick={() => setShowNavDropdown(!showNavDropdown)}
           role="button"
-          onKeyDown={() => setShowNav(!showNav)}
+          onKeyDown={() => setShowNavDropdown(!showNavDropdown)}
+          // The tabindex global attribute indicates that its element can be focused,
+          // and where it exists in sequential keyboard navigation.
           tabIndex={0}
         >
           <MdClose />
-        </div>
-        <li>
+        </CloseNav>
+        <NavItem>
           <NavLink
             to="/"
             exact
-            onClick={() => setShowNav(!showNav)}
+            onClick={() => setShowNavDropdown(!showNavDropdown)}
             role="button"
-            onKeyDown={() => setShowNav(!showNav)}
+            onKeyDown={() => setShowNavDropdown(!showNavDropdown)}
             tabIndex={0}
           >
             Home
           </NavLink>
-        </li>
-        <li>
+        </NavItem>
+        <NavItem>
           <NavLink
             to="/about"
-            onClick={() => setShowNav(!showNav)}
+            onClick={() => setShowNavDropdown(!showNavDropdown)}
             role="button"
-            onKeyDown={() => setShowNav(!showNav)}
+            onKeyDown={() => setShowNavDropdown(!showNavDropdown)}
             tabIndex={0}
           >
             About
           </NavLink>
-        </li>
-        <li>
+        </NavItem>
+        <NavItem>
           <NavLink
             to="/projects"
-            onClick={() => setShowNav(!showNav)}
+            onClick={() => setShowNavDropdown(!showNavDropdown)}
             role="button"
-            onKeyDown={() => setShowNav(!showNav)}
+            onKeyDown={() => setShowNavDropdown(!showNavDropdown)}
             tabIndex={0}
           >
             Projects
           </NavLink>
-        </li>
-        <li>
+        </NavItem>
+        <NavItem>
           <NavLink
             to="/contact"
-            onClick={() => setShowNav(!showNav)}
+            onClick={() => setShowNavDropdown(!showNavDropdown)}
             role="button"
-            onKeyDown={() => setShowNav(!showNav)}
+            onKeyDown={() => setShowNavDropdown(!showNavDropdown)}
             tabIndex={0}
           >
             Contact
           </NavLink>
-        </li>
-      </ul>
+        </NavItem>
+      </NavItems>
     </NavStyles>
   );
 };
